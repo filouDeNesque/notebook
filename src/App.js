12b07660ba14f862bc8bc73f93daa07cd14c3bd7
FilouDeNesque <johndoe@example.com>
2021-08-25 14:30:38 +0200

    mise au propre de la structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
- import React, { useRef } from 'react';
- import { Editor } from '@tinymce/tinymce-react';
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import "./App.css";
 
- export default function App() {
-   const editorRef = useRef(null);
-   const log = () => {
-     if (editorRef.current) {
-       console.log(editorRef.current.getContent());
-     }
-   };
-   return (
-     <>
-       <Editor
-         onInit={(evt, editor) => editorRef.current = editor}
-         initialValue="<p>This is the initial content of the editor.</p>"
-     inline
-         init={{
-           height: 500,
-           menubar: true,
-           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-         }}
-       />
-       <button onClick={log}>Log editor content</button>
-     </>
-   );
- }
+
+export default function App() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+
+  //create new file or read new file
+  //save content to this file
+  // liste des fichiers enregistrer
+
+  return (
+    <>
+    <Editor
+    onInit={(evt, editor) => editorRef.current = editor}
+    initialValue="<p>This is the initial content of the editor.</p>"
+    inline
+    init={{
+      height: 500,
+	menubar: true,
+	content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+    }}
+    />
+    <button onClick={log}>Log editor content</button>
+    </>
+  );
+}
